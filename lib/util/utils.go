dd0364f3762c81e86450e08d31c2a75f99dafbc9
Alex Bramley <a.bramley@gmail.com>
2011-07-28 19:17:24 +0100

    Refactor RemovePrefixedNick to allow for just testing for a prefix.

--- lib/util/utils.go
+++ lib/util/utils.go
@@ -6,17 +6,24 @@ import (
 	"strings"
 )
 
-func RemovePrefixedNick(text, nick string) string {
+func RemovePrefixedNick(text, nick string) (string, bool) {
+	if HasPrefixedNick(text, nick) {
+		text = strings.TrimSpace(text[len(nick)+1:])
+		return text, true
+	}
+	return text, false
+}
+
+func HasPrefixedNick(text, nick string) bool {
+	prefixed := false
 	if strings.HasPrefix(strings.ToLower(text), strings.ToLower(nick)) {
-		l := len(nick)
-		switch text[l] {
+		switch text[len(nick)] {
 		// This is nicer than an if statement :-)
 		// We only cut off the nick if it's followed by one of these chars
 		// and an optional space, to indicate that it was prefixed to the text.
 		case ':', ';', ',', '>', '-':
-			l++
-			text = strings.TrimSpace(text[l:])
+			prefixed = true
 		}
 	}
-	return text
+	return prefixed
 }
