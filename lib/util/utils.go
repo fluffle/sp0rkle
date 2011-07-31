a32f6d2680ec5c790ff3c22a074a6e7554619082
Alex Bramley <a.bramley@gmail.com>
2011-07-31 18:18:21 +0100

    Move NewRand() and associated into separate file.

--- lib/util/utils.go
+++ lib/util/utils.go
@@ -4,8 +4,6 @@ package util
 
 import (
 	"strings"
-	"rand"
-	"sync"
 )
 
 func RemovePrefixedNick(text, nick string) (string, bool) {
@@ -84,26 +82,4 @@ func LooksURLish(s string) bool {
 	return ((strings.HasPrefix(s, "http://") ||
 		strings.HasPrefix(s, "https://")) &&
 		strings.Index(s, " ") == -1)
-}
-
-// Gratuitously stolen from pkg/rand, cos they aren't usable externally.
-type lockedSource struct {
-	sync.Mutex
-	rand.Source
-}
-
-func (r *lockedSource) Int63() (n int64) {
-	r.Lock()
-	defer r.Unlock()
-	return r.Source.Int63()
-}
-
-func (r *lockedSource) Seed(seed int64) {
-	r.Lock()
-	r.Source.Seed(seed)
-	r.Unlock()
-}
-
-func NewRand(seed int64) *rand.Rand {
-	return rand.New(&lockedSource{Source: rand.NewSource(seed)})
 }
