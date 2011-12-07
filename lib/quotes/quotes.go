215720f66722a74bfc35c569edadb41a6e7ad3ec
Alex Bramley <a.bramley@gmail.com>
2011-12-07 22:32:50 +0000

    Pseudo-random quote lookups a-la factoid lookups.

--- lib/quotes/quotes.go
+++ lib/quotes/quotes.go
@@ -30,6 +30,9 @@ type QuoteCollection struct {
 	// Wrap mgo.Collection
 	mgo.Collection
 
+	// Cache of ObjectId's for PseudoRand
+	seen map[string][]bson.ObjectId
+
 	// logging object
 	l logging.Logger
 }
@@ -37,6 +40,7 @@ type QuoteCollection struct {
 func Collection(dbh *db.Database, l logging.Logger) *QuoteCollection {
 	qc := &QuoteCollection{
 		Collection: dbh.C(COLLECTION),
+		seen:       make(map[string][]bson.ObjectId),
 		l:          l,
 	}
 	err := qc.EnsureIndex(mgo.Index{Key: []string{"qid"}})
@@ -54,14 +58,31 @@ func (qc *QuoteCollection) GetByQID(qid int) *Quote {
 	return nil
 }
 
-func (qc *QuoteCollection) GetQuoteMatching(regex string) *Quote {
-	query := qc.Find(bson.M{"quote": bson.M{"$regex": regex}})
+// TODO(fluffle): reduce duplication with lib/factoids?
+func (qc *QuoteCollection) GetPseudoRand(regex string) *Quote {
+	lookup := bson.M{}
+	if regex != "" {
+		// Only perform a regex lookup if there's a regex to match against,
+		// otherwise this just fetches a quote at pseudo-random.
+		lookup["quote"] = bson.M{"$regex": regex, "$options": "i"}
+	}
+	ids, ok := qc.seen[regex]
+	if ok && len(ids) > 0 {
+		qc.l.Debug("Looked for quotes matching '%s' before, %d stored id's",
+			regex, len(ids))
+		lookup["_id"] = bson.M{"$nin": ids}
+	}
+	query := qc.Find(lookup)
 	count, err := query.Count()
 	if err != nil {
 		qc.l.Warn("Count for quote lookup '%s' failed: %s", regex, err)
 		return nil
 	}
 	if count == 0 {
+		if ok {
+			// Looked for this regex before, but nothing matches now
+			qc.seen[regex] = nil, false
+		}
 		return nil
 	}
 	var res Quote
@@ -71,6 +92,20 @@ func (qc *QuoteCollection) GetQuoteMatching(regex string) *Quote {
 	if err = query.One(&res); err != nil {
 		qc.l.Warn("Fetch for quote lookup '%s' failed: %s", regex, err)
 		return nil
+	}
+	if count != 1 {
+		if !ok {
+			// only store seen for regex that match more than one quote
+			qc.l.Debug("Creating seen data for regex '%s'.", regex)
+			qc.seen[regex] = make([]bson.ObjectId, 0, count)
+		}
+		qc.l.Debug("Storing id %v for regex '%s'.", res.Id, regex)
+		qc.seen[regex] = append(qc.seen[regex], res.Id)
+	} else if ok {
+		// if the count of results is 1 and we're storing seen data for regex
+		// then we've exhausted the possible results and should wipe it
+		qc.l.Debug("Zeroing seen data for regex '%s'.", regex)
+		qc.seen[regex] = nil, false
 	}
 	return &res
 }
