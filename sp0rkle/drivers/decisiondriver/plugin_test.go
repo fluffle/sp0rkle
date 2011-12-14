d0fa88802b0f2697a24c23f8072f5980ec3743e3
Alex Bramley <a.bramley@gmail.com>
2011-12-14 00:15:16 +0000

    Unit test for decisiondriver's choices().

--- sp0rkle/drivers/decisiondriver/plugin_test.go
+++ sp0rkle/drivers/decisiondriver/plugin_test.go
@@ -1,9 +1,10 @@
 package decisiondriver
 
 import (
-	"rand"
-	"testing"
 	"lib/util"
+	"rand"
+	"reflect"
+	"testing"
 )
 
 // deterministic randomizer
@@ -61,6 +62,41 @@ func TestDecide(t *testing.T) {
 		ret := rand_decider(s, mytestrand)
 		if ret != expected[i] {
 			t.Errorf("Test: %s\nExpected: [%s]\nGot: [%s]\n\n", s, expected[i], ret)
+		}
+	}
+}
+
+func TestChoices(t *testing.T) {
+	tests := []string{
+		"singlevalue",
+		"AAA BBB CCC",
+		"DDD | EEE",
+		"FFF | GGG | HHH",
+		"spam | spam and sausage | eggs | ham | spam eggs and spam",
+		`"spam" "spam and sausage" "eggs" "ham" "spam spam spam spam eggs and spam"`,
+		`"cheese" "ham`,
+		`'cheese' 'carrots' 'sausage'`,
+		`"foo bar" "foo's bar" "something with spaces in it"`,
+		`"foobar" "bar" "cheese"`,
+	}
+	expected := [][]string{
+		[]string{"singlevalue"},
+		[]string{"AAA", "BBB", "CCC"},
+		[]string{"DDD ", " EEE"},
+		[]string{"FFF ", " GGG ", " HHH"},
+		[]string{"spam ", " spam and sausage ", " eggs ", " ham ", " spam eggs and spam"},
+		[]string{"spam", "spam and sausage", "eggs", "ham", "spam spam spam spam eggs and spam"},
+		[]string{"Unbalanced quotes"},
+		[]string{"cheese", "carrots", "sausage"},
+		[]string{"foo bar", "foo's bar", "something with spaces in it"},
+		[]string{"foobar", "bar", "cheese"},
+	}
+	for i, s := range tests {
+		ret := choices(s)
+		// We don't trim space from the possible choices *in* choices()
+		// as it's only really necessary to do so for the one chosen.
+		if !reflect.DeepEqual(expected[i], ret) {
+			t.Errorf("Test: %s\nExpected: %#v\nGot: %#v\n\n", s, expected[i], ret)
 		}
 	}
 }
