bc283489af3bb8f50f9f25d2bf30a79125ba9d55
Alex Bramley <a.bramley@gmail.com>
2012-10-14 15:49:26 +0100

    Gut netdriver until useful features arrive.

--- drivers/netdriver/httpservice.go
+++ drivers/netdriver/httpservice.go
@@ -2,11 +2,9 @@ package netdriver
 
 import (
 	"fmt"
-	"github.com/fluffle/golog/logging"
 	"io/ioutil"
 	"net/http"
 	"net/url"
-	"regexp"
 )
 
 func get(req string) (string, bool) {
@@ -24,39 +22,9 @@ func get(req string) (string, bool) {
 
 type httpService struct {
 	uri string
-	l   logging.Logger
 }
 
 func (hs *httpService) LookupResult(req string) string {
 	ret, _ := get(fmt.Sprintf(hs.uri, url.QueryEscape(req)))
 	return ret
-}
-
-type iGoogleCalcService httpService
-
-func IGoogleCalcService(l logging.Logger) *iGoogleCalcService {
-	return &iGoogleCalcService{
-		uri: "http://www.google.com/ig/calculator?hl=en&q=%s",
-		l: l,
-	}
-}
-
-func (is *iGoogleCalcService) LookupResult(q string) string {
-	data, ok := get(fmt.Sprintf(is.uri, url.QueryEscape(q)))
-	is.l.Info("Got '%s' for query '%s'", data, q)
-	if !ok {
-		return data
-	}
-	// Irritatingly, the iGoogle calculator sends back malformed JSON
-	// where the object keys aren't quoted, so we do this manually.
-	// The format is: {lhs: "2 + 2",rhs: "4",error: "",icc: false}
-	rx := regexp.MustCompile(`rhs: "([^"]*)",error: "([^"]*)"`)
-	res := rx.FindStringSubmatch(data)
-	if res == nil {
-		return fmt.Sprintf("Regex error: no match in '%s'", data)
-	}
-	if res[2] != "" {
-		return res[2] // err
-	}
-	return res[1] // rhs
 }
--- drivers/netdriver/netdriver.go
+++ drivers/netdriver/netdriver.go
@@ -1,26 +1,11 @@
 package netdriver
 
 import (
-	"github.com/fluffle/golog/logging"
 )
-
-const driverName string = "net"
 
 type netService interface {
 	LookupResult(string) string
 }
 
-type netDriver struct {
-	services map[string]netService
-	l logging.Logger
-}
-
-func NetDriver(l logging.Logger) *netDriver {
-	nd := &netDriver{make(map[string]netService), l}
-//	nd.services["calc"] = IGoogleCalcService(l)
-	return nd
-}
-
-func (nd *netDriver) Name() string {
-	return driverName
+func Init() {
 }
