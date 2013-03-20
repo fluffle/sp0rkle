7a2e87f365942094f0e267121341a23ab3a74af0
Alex Bramley <a.bramley@gmail.com>
2013-03-20 06:21:50 +0000

    UD has a JSON API, so ditch my horrible HTML parsing crap \o/

--- drivers/netdriver/commands.go
+++ drivers/netdriver/commands.go
@@ -1,72 +1,98 @@
 package netdriver
 
 import (
-	"encoding/xml"
+	"encoding/json"
 	"fmt"
+	"github.com/fluffle/golog/logging"
 	"github.com/fluffle/sp0rkle/bot"
 	"net/url"
 	"strings"
+	"time"
 )
 
-const udUrl = "http://www.urbandictionary.com/define.php?term=%s"
+const udUrl = "http://api.urbandictionary.com/v0/define?term=%s"
+// TODO(fluffle): Put this in util and clean up the various copies of it.
+const TimeFormat = "15:04:05, Monday 2 January 2006"
 
-func urbanDictionary(ctx *bot.Context) {
-	uri := fmt.Sprintf(udUrl, url.QueryEscape(ctx.Text()))
-	d, err := get(uri)
-	if err != nil {
-		ctx.ReplyN("GET failed: %v", err)
+// These shamelessly stolen from StalkR:
+// https://github.com/StalkR/misc/blob/master/urbandictionary/urbandictionary.go
+type udResult struct {
+	Type       string   `json:"result_type"`
+	HasRelated bool     `json:"has_related_words"`
+	Pages      int      `json:"pages,omitempty"`
+	Total      int      `json:"total,omitempty"`
+	Sounds     []string `json:"sounds,omitempty"`
+	List       []udDef  `json:"list"`
+}
+
+type udDef struct {
+	Word        string `json:"word"`
+	Definition  string `json:"definition"`
+	Example     string `json:"example"`
+	Author      string `json:"author"`
+	Id          int    `json:"defid"`
+	Url         string `json:"permalink"`
+	Vote        string `json:"current_vote"`
+	Upvotes     int    `json:"thumbs_up"`
+	Downvotes   int    `json:"thumbs_down"`
+	Term        string `json:"term,omitempty"`
+	Type        string `json:"type,omitempty"`
+}
+
+type udCacheEntry struct {
+	result    *udResult
+	stamp     time.Time
+}
+
+type udCache map[string]udCacheEntry
+
+func (udc udCache) prune() {
+	for k, v := range udc {
+		if time.Since(v.stamp) > 24 * time.Hour {
+			delete(udc, k)
+		}
+	}
+}
+
+func (udc udCache) fetch(term string) (entry udCacheEntry, ok bool, err error) {
+	udc.prune()
+	entry, ok = udc[term]
+	if ok { return }
+	entry.result = &udResult{}
+	data, err := get(fmt.Sprintf(udUrl, url.QueryEscape(term)))
+	if err != nil { return }
+	if err = json.Unmarshal(data, entry.result); err != nil {
+		logging.Debug("JSON: %s", data)
 		return
 	}
-	// Parsing HTML with encoding/xml is a bit meh.
-	// First skip to <div class="definition">, then ...
-	for {
-		tok, err := d.Token()
-		if err != nil {
-			ctx.ReplyN("HTML parse error: %v", err)
-			break
-		}
-		se, ok := tok.(xml.StartElement)
-		if ok &&
-			se.Name.Local == "div" &&
-			len(se.Attr) == 1 &&
-			(se.Attr[0].Value == "definition" ||
-			se.Attr[0].Value == "not_defined_yet") { break }
+	// Abuse Pages and Total for our own ends here
+	entry.result.Pages, entry.result.Total = -1, len(entry.result.List)
+	entry.stamp = time.Now()
+	udc[term] = entry
+	return
+}
+
+var cache = udCache{}
+
+func urbanDictionary(ctx *bot.Context) {
+	entry, ok, err := cache.fetch(strings.ToLower(ctx.Text()))
+	if err != nil {
+		ctx.ReplyN("ud request failed: %#v", err)
+		return
 	}
-	// ... the next token should be the start of the definition.
-	// At this point we assemble a slice of strings containing the
-	// text inside this div from the xml.CharData tokens, bolding
-	// everything inside <a> tags as other definable names.
-	defn := []string{}
-LOOP:
-	for {
-		t, err := d.Token()
-		if err != nil {
-			ctx.ReplyN("HTML parse error: %v", err)
-			break
-		}
-		switch tok := t.(type) {
-		case xml.CharData:
-			// These can contain newlines, so turn them to spaces.
-			s := strings.Replace(string(tok), "\n", " ", -1)
-			defn = append(defn, s)
-		case xml.StartElement:
-			switch tok.Name.Local {
-			case "a":
-				// <a>
-				defn = append(defn, "\x02")
-			case "br":
-				defn = append(defn, " ")
-			}
-		case xml.EndElement:
-			switch tok.Name.Local {
-			case "a":
-				// </a>
-				defn = append(defn, "\x02")
-			case "div":
-				// </div>; bail out
-				break LOOP
-			}
-		}
+	cached, r := "", entry.result
+	if ok {
+		cached = fmt.Sprintf(", result cached at %s",
+			entry.stamp.Format(TimeFormat))
 	}
-	ctx.Reply(strings.Join(defn, ""))
+	if r.Total == 0 || r.Type == "no_results" {
+		ctx.ReplyN("%s isn't defined yet%s.", ctx.Text(), cached)
+		return
+	}
+	// Cycle through all the definitions on repeated calls for the same term
+	r.Pages = (r.Pages + 1) % r.Total
+	def := r.List[r.Pages]
+	ctx.Reply("[%d/%d] %s (%d up, %d down%s)", r.Pages + 1, r.Total,
+		strings.Replace(def.Definition, "\r\n", " ", -1),
+		def.Upvotes, def.Downvotes, cached)
 }
--- drivers/netdriver/netdriver.go
+++ drivers/netdriver/netdriver.go
@@ -1,21 +1,18 @@
 package netdriver
 
 import (
-	"encoding/xml"
 	"github.com/fluffle/sp0rkle/bot"
+	"io/ioutil"
 	"net/http"
 )
 
-func get(req string) (*xml.Decoder, error) {
+func get(req string) ([]byte, error) {
 	res, err := http.Get(req)
 	if err != nil {
 		return nil, err
 	}
-	d := xml.NewDecoder(res.Body)
-	d.Strict = false
-	d.AutoClose = xml.HTMLAutoClose
-	d.Entity = xml.HTMLEntity
-	return d, nil
+	defer res.Body.Close()
+	return ioutil.ReadAll(res.Body)
 }
 
 func Init() {
