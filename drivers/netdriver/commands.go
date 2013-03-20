70160fd06d4cf703be94ed5120bc15fe0c02a1b0
Alex Bramley <a.bramley@gmail.com>
2013-03-20 06:32:46 +0000

    Support for basic GitHub bug filing. Fixes #8.

--- drivers/netdriver/commands.go
+++ drivers/netdriver/commands.go
@@ -3,6 +3,8 @@ package netdriver
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/fluffle/go-github-client/client"
+	"github.com/fluffle/go-github-client/issues"
 	"github.com/fluffle/golog/logging"
 	"github.com/fluffle/sp0rkle/bot"
 	"net/url"
@@ -95,4 +97,39 @@ func urbanDictionary(ctx *bot.Context) {
 	ctx.Reply("[%d/%d] %s (%d up, %d down%s)", r.Pages + 1, r.Total,
 		strings.Replace(def.Definition, "\r\n", " ", -1),
 		def.Upvotes, def.Downvotes, cached)
+}
+
+func createGitHubIssue(ctx *bot.Context) {
+	if *githubToken == "" {
+		ctx.ReplyN("I don't have a GitHub API token, sorry.")
+		return
+	}
+	s := strings.SplitN(ctx.Text(), ". ", 2)
+	if s[0] == "" {
+		ctx.ReplyN("I'm not going to create an empty issue.")
+		return
+	}
+	ghc, _ := client.NewGithubClient("", *githubToken, client.AUTH_OAUTH2_TOKEN)
+	ic := issues.NewIssues(ghc)
+	issue := &issues.IssueDataCreate{
+		Title:    s[0]+".",
+		Assignee: "fluffle",
+		Labels:   []string{"from:IRC", "nick:"+ctx.Nick, "chan:"+ctx.Target()},
+	}
+	if len(s) == 2 {
+		issue.Body = s[1]
+	}
+	res, err := ic.CreateIssue("fluffle", "sp0rkle", issue)
+	if err != nil {
+		ctx.ReplyN("Error creating issue: %v", err)
+		return
+	}
+	data, err := res.JsonMap()
+	if err != nil {
+		ctx.ReplyN("Error unmarshalling JSON response: %v", err)
+		return
+	}
+	logging.Info("%#v", data)
+	ctx.ReplyN("Issue #%d created at %s",
+		data.GetInt("number"), data.GetString("html_url"))
 }
--- drivers/netdriver/netdriver.go
+++ drivers/netdriver/netdriver.go
@@ -1,10 +1,14 @@
 package netdriver
 
 import (
+	"flag"
 	"github.com/fluffle/sp0rkle/bot"
 	"io/ioutil"
 	"net/http"
 )
+
+var githubToken = flag.String("github_token", "",
+	"OAuth2 token for accessing the GitHub API.")
 
 func get(req string) ([]byte, error) {
 	res, err := http.Get(req)
@@ -18,4 +22,6 @@ func get(req string) ([]byte, error) {
 func Init() {
 	bot.Command(urbanDictionary, "ud", "ud <term>  -- "+
 		"Look up <term> on UrbanDictionary.")
+	bot.Command(createGitHubIssue, "file bug:", "file bug: <title>. "+
+		"<descriptive body>  -- Files a bug on GitHub. Abusers will be hurt.")
 }
