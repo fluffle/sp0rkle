f4faae3e832109594f8fe12e288f4d16dbba944d
Alex Bramley <a.bramley@gmail.com>
2013-03-24 13:02:12 +0000

    Fix bug that causes repeat URLs to try to insert dup keys.

--- drivers/urldriver/handlers.go
+++ drivers/urldriver/handlers.go
@@ -13,12 +13,12 @@ func urlScan(ctx *bot.Context) {
 	n, c := ctx.Storable()
 	for _, w := range words {
 		if util.LooksURLish(w) {
-			if u := uc.GetByUrl(w); u != nil &&
-				u.Nick != bot.Nick(ctx.Nick) &&
-				time.Since(u.Timestamp) > 2*time.Hour {
-				ctx.Reply("that URL first mentioned by %s %s ago",
-					u.Nick, util.TimeSince(u.Timestamp))
-
+			if u := uc.GetByUrl(w); u != nil {
+				if u.Nick != bot.Nick(ctx.Nick) &&
+					time.Since(u.Timestamp) > 2*time.Hour {
+					ctx.Reply("that URL first mentioned by %s %s ago",
+						u.Nick, util.TimeSince(u.Timestamp))
+				}
 				continue
 			}
 			u := urls.NewUrl(w, n, c)
