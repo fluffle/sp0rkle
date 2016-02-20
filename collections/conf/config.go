63c6381f67a1d3b6dac7b4499e9b52bfa80a9b15
Alex Bramley <a.bramley@gmail.com>
2016-02-20 15:51:02 +0000

    Re-implement conf collection to use both DBs.

--- collections/conf/config.go
+++ collections/conf/config.go
@@ -2,125 +2,55 @@ package conf
 
 import (
 	"fmt"
-	"sync"
 
-	"github.com/fluffle/golog/logging"
+	"github.com/fluffle/goirc/logging"
 	"github.com/fluffle/sp0rkle/db"
 	"gopkg.in/mgo.v2"
-	"gopkg.in/mgo.v2/bson"
 )
 
 const COLLECTION string = "conf"
 
-type namespace string
+var mongo db.C
 
-type Namespace interface {
-	All() []Entry
-	String(key string, value ...string) string
-	Int(key string, value ...int) int
-	Float(key string, value ...float64) float64
-	Value(key string, value ...interface{}) interface{}
-	Delete(key string)
+func mongoIndexes(c db.Collection) {
+	err := c.Mongo().EnsureIndex(mgo.Index{Key: []string{"ns", "key"}, Unique: true})
+	if err != nil {
+		logging.Error("Couldn't create index on sp0rkle.conf: %s", err)
+	}
+	// Dirty hacks to migrate data.
+	var all []Entry
+	mongo.All(db.K{}, &all)
+	for _, e := range all {
+		logging.Debug("Migrating entry %s.", e)
+		Bolt(e.Ns).Value(e.Key, e.Value)
+	}
+}
+
+func Mongo(ns string) *namespace {
+	mongo.Init(db.Mongo, COLLECTION, mongoIndexes)
+	return &namespace{ns: ns, Collection: &mongo}
+}
+
+var bolt db.C
+
+func Bolt(ns string) *namespace {
+	bolt.Init(db.Bolt, COLLECTION, nil)
+	return &namespace{ns: ns, Collection: &bolt}
+}
+
+func Ns(ns string) *both {
+	return &both{bolt: Bolt(ns), mongo: Mongo(ns)}
 }
 
 type Entry struct {
-	Ns    namespace
-	Key   string
-	Value interface{}
+	Ns, Key string
+	Value   interface{}
 }
 
 func (e Entry) String() string {
 	return fmt.Sprintf("%s<%s: %v>", e.Ns, e.Key, e.Value)
 }
 
-func (e Entry) Id() bson.M {
-	return bson.M{"ns": e.Ns, "key": e.Key}
-}
-
-var conf *mgo.Collection
-var lock sync.Mutex
-
-func Ns(ns string) namespace {
-	lock.Lock()
-	defer lock.Unlock()
-	if conf == nil {
-		conf = db.Init().C(COLLECTION)
-		err := conf.EnsureIndex(mgo.Index{Key: []string{"ns", "key"}, Unique: true})
-		if err != nil {
-			logging.Error("Couldn't create index on sp0rkle.conf: %s", err)
-		}
-	}
-	return namespace(ns)
-}
-
-func (ns namespace) key(key string) bson.M {
-	return bson.M{"ns": ns, "key": key}
-}
-
-func (ns namespace) set(key string, value interface{}) {
-	e := Entry{Ns: ns, Key: key, Value: value}
-	if _, err := conf.Upsert(e.Id(), e); err != nil {
-		logging.Error("Couldn't upsert config entry '%s': %s", e, err)
-	}
-}
-
-func (ns namespace) get(key string) interface{} {
-	var e Entry
-	if err := conf.Find(ns.key(key)).One(&e); err == nil {
-		return e.Value
-	}
-	return nil
-}
-
-func (ns namespace) All() []Entry {
-	var e []Entry
-	if err := conf.Find(bson.M{"ns": ns}).All(&e); err == nil {
-		return e
-	}
-	return []Entry{}
-}
-
-func (ns namespace) String(key string, value ...string) string {
-	if len(value) > 0 {
-		ns.set(key, value[0])
-		return value[0]
-	}
-	if val, ok := ns.get(key).(string); ok {
-		return val
-	}
-	return ""
-}
-
-func (ns namespace) Int(key string, value ...int) int {
-	if len(value) > 0 {
-		ns.set(key, value[0])
-		return value[0]
-	}
-	if val, ok := ns.get(key).(int); ok {
-		return val
-	}
-	return 0
-}
-
-func (ns namespace) Float(key string, value ...float64) float64 {
-	if len(value) > 0 {
-		ns.set(key, value[0])
-		return value[0]
-	}
-	if val, ok := ns.get(key).(float64); ok {
-		return val
-	}
-	return 0
-}
-
-func (ns namespace) Value(key string, value ...interface{}) interface{} {
-	if len(value) > 0 {
-		ns.set(key, value[0])
-		return value[0]
-	}
-	return ns.get(key)
-}
-
-func (ns namespace) Delete(key string) {
-	conf.Remove(ns.key(key))
+func (e Entry) K() db.K {
+	return db.K{{"ns", e.Ns}, {"key", e.Key}}
 }
