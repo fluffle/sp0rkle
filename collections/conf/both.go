6a92e865b74c5513ba5a23802927eb6d8f9d08f2
Alex Bramley <a.bramley@gmail.com>
2016-02-22 20:28:44 +0000

    Stricter migration with diff checking.

--- collections/conf/both.go
+++ collections/conf/both.go
@@ -2,12 +2,73 @@ package conf
 
 import (
 	"reflect"
+	"sort"
+	"strings"
+	"sync"
 
 	"github.com/fluffle/golog/logging"
+	"github.com/fluffle/sp0rkle/db"
+	"github.com/fluffle/sp0rkle/util/diff"
 )
+
+type migrator struct {
+	sync.RWMutex
+	migrated bool
+}
+
+func (m *migrator) Migrate() error {
+	m.Lock()
+	defer m.Unlock()
+	var all []Entry
+	mongo.Init(db.Mongo, COLLECTION, mongoIndexes)
+	if err := mongo.All(db.K{}, &all); err != nil {
+		return err
+	}
+	for _, e := range all {
+		logging.Debug("Migrating entry %s.", e)
+		Bolt(e.Ns).Value(e.Key, e.Value)
+	}
+	if err := m.diff(); err != nil {
+		return err
+	}
+	m.migrated = true
+	return nil
+}
+
+func (m *migrator) diff() error {
+	var mAll, bAll []Entry
+	if err := mongo.All(db.K{}, &mAll); err != nil {
+		return err
+	}
+	if err := bolt.All(db.K{}, &bAll); err != nil {
+		return err
+	}
+	mStr, bStr := make([]string, len(mAll)), make([]string, len(bAll))
+	for i, e := range mAll {
+		mStr[i] = e.String()
+	}
+	for i, e := range bAll {
+		bStr[i] = e.String()
+	}
+	sort.Strings(mStr)
+	sort.Strings(bStr)
+	if len(mAll) != len(bAll) || strings.Join(mStr, "\n") != strings.Join(bStr, "\n") {
+		logging.Error("Diff: mlen = %d, blen = %d\n%s\n", len(mAll), len(bAll),
+			strings.Join(diff.Unified(mStr, bStr), "\n"))
+		return diff.ErrDiff
+	}
+	return nil
+}
+
+func (m *migrator) Migrated() bool {
+	m.RLock()
+	defer m.RUnlock()
+	return m.migrated
+}
 
 type both struct {
 	bolt, mongo *namespace
+	*migrator
 }
 
 func (b both) All() []Entry {
@@ -16,6 +77,9 @@ func (b both) All() []Entry {
 	if !reflect.DeepEqual(mongo, bolt) {
 		logging.Warn("All() mismatch (%v vs. %v) for ns %q.",
 			mongo, bolt, b.mongo.ns)
+	}
+	if b.Migrated() {
+		return bolt
 	}
 	return mongo
 }
@@ -27,6 +91,9 @@ func (b both) String(key string, value ...string) string {
 		logging.Warn("String() mismatch (%q vs. %q) for ns %q, key %q.",
 			mongo, bolt, b.mongo.ns, key)
 	}
+	if b.Migrated() {
+		return bolt
+	}
 	return mongo
 }
 
@@ -36,6 +103,9 @@ func (b both) Int(key string, value ...int) int {
 	if mongo != bolt {
 		logging.Warn("Int() mismatch (%d vs. %d) for ns %q, key %q.",
 			mongo, bolt, b.mongo.ns, key)
+	}
+	if b.Migrated() {
+		return bolt
 	}
 	return mongo
 }
@@ -47,6 +117,9 @@ func (b both) Float(key string, value ...float64) float64 {
 		logging.Warn("Float() mismatch (%f vs. %f) for ns %q, key %q.",
 			mongo, bolt, b.mongo.ns, key)
 	}
+	if b.Migrated() {
+		return bolt
+	}
 	return mongo
 }
 
@@ -56,6 +129,9 @@ func (b both) Value(key string, value ...interface{}) interface{} {
 	if !reflect.DeepEqual(mongo, bolt) {
 		logging.Warn("Value() mismatch (%v vs. %v) for ns %q, key %q.",
 			mongo, bolt, b.mongo.ns, key)
+	}
+	if b.Migrated() {
+		return bolt
 	}
 	return mongo
 }
