06df9b9eedf1e348212e2a578b082574f8da4198
Alex Bramley <a.bramley@gmail.com>
2016-02-22 00:11:04 +0000

    Patience diff, from bazaar's bzrlib/_patiencediff_py.py
    http://bazaar.launchpad.net/~bzr-pqm/bzr/bzr.dev/view/head:/bzrlib/_patiencediff_py.py
    
    Needs some tidying.

---
+++ util/diff/patience_test.go
@@ -0,0 +1,102 @@
+package diff
+
+import (
+	"strings"
+	"testing"
+)
+
+// This is the "canonical" patience diff example.
+func TestUnifiedDiff(t *testing.T) {
+	before := `#include <stdio.h>
+
+// Frobs foo heartily
+int frobnitz(int foo)
+{
+    int i;
+    for(i = 0; i < 10; i++)
+    {
+        printf("Your answer is: ");
+        printf("%d\n", foo);
+    }
+}
+
+int fact(int n)
+{
+    if(n > 1)
+    {
+        return fact(n-1) * n;
+    }
+    return 1;
+}
+
+int main(int argc, char **argv)
+{
+    frobnitz(fact(10));
+}`
+	after := `#include <stdio.h>
+
+int fib(int n)
+{
+    if(n > 2)
+    {
+        return fib(n-1) + fib(n-2);
+    }
+    return 1;
+}
+
+// Frobs foo heartily
+int frobnitz(int foo)
+{
+    int i;
+    for(i = 0; i < 10; i++)
+    {
+        printf("%d\n", foo);
+    }
+}
+
+int main(int argc, char **argv)
+{
+    frobnitz(fib(10));
+}`
+	want := ` #include <stdio.h>
+ 
++int fib(int n)
++{
++    if(n > 2)
++    {
++        return fib(n-1) + fib(n-2);
++    }
++    return 1;
++}
++
+ // Frobs foo heartily
+ int frobnitz(int foo)
+ {
+     int i;
+     for(i = 0; i < 10; i++)
+     {
+-        printf("Your answer is: ");
+         printf("%d\n", foo);
+     }
+ }
+ 
+-int fact(int n)
+-{
+-    if(n > 1)
+-    {
+-        return fact(n-1) * n;
+-    }
+-    return 1;
+-}
+-
+ int main(int argc, char **argv)
+ {
+-    frobnitz(fact(10));
++    frobnitz(fib(10));
+ }`
+	a, b := strings.Split(before, "\n"), strings.Split(after, "\n")
+	got := strings.Join(Unified(a, b), "\n")
+	if got != want {
+		t.Errorf("Did not get expected diff.\n\nGOT:\n\n%s\nWANT:\n\n%s", got, want)
+	}
+}
