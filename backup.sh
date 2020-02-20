b30f4a380cc6cecde47aa6ab088942b9348d4b80
Alex Bramley <a.bramley@gmail.com>
2020-02-20 15:02:07 +0000

    Restore notes in backup script.

--- backup.sh
+++ backup.sh
@@ -13,3 +13,8 @@ trap "/bin/rm -r \"$TMPDIR\"; exit" INT TERM EXIT
 DATE="$(date "+%Y-%m-%d.%H:%M")"
 /usr/bin/mongodump -d sp0rkle -o "$TMPDIR" >/dev/null
 /bin/tar -C "$TMPDIR" -cjf "$BACKUPDIR/sp0rkle.$DATE.tar.bz2" .
+
+# To restore into bitnami mongodb docker image in k8s:
+#  - untar to persistent volume so container can see backup
+#  - docker exec -t <container> /opt/bitnami/mongodb/bin/mongorestore \
+#        --drop -d sp0rkle --dir=/bitnami/mongodb/restore/sp0rkle -v
